92ba025f1c3492a17a3b2e4ffc39a38de63cd885
ksd3 <89213519+ksd3@users.noreply.github.com>
2023-01-29 07:21:55 -0500

    Add files via upload

---
+++ beatqraft/routes.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template, jsonify
+from beatqraft.get_beat import get_beat_strings
+from beatqraft.beatgenerator import create_midi_file
+
+
+# Blueprint Configuration
+
+main_bp = Blueprint(
+    'main_bp', __name__,
+    template_folder='templates',
+    static_folder='static'
+)
+
+@main_bp.route('/')
+def index():
+    return render_template('index.html')
+
+
+@main_bp.route('/api/a', methods=['GET'])
+def get_a():
+    hihat_beat, snare_beat, bass_beat = get_beat_strings()
+    create_midi_file(bass_beat, snare_beat, hihat_beat)
+    return jsonify({'status': 'success', 'result': 123})
+
+
+
